8d91b5912ba455d672b42cad33274775e70b77db
lacibe2 <lara27cibelly@gmail.com>
2024-06-24 23:50:11 -0300

    implementação do Olá, Mundo robusto

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Welcome {
     public static void main (String[]args){
+        Scanner entrada = new Scanner(System.in);
+        String nome;
         System.out.println("Olá Mundo");
+        System.out.println("Qual o seu nome");
+        nome = entrada.nextLine();
+        System.out.println("Olá  " +nome +" seja bem-vindo ao curso de git");
+
 
     }
 }
